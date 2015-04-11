a1daaff343ea7afa9e144876733ce4029dde441d
Olivier Thivierge <account2707@hotmail.com>
2015-04-11 11:09:26 -0500

    7
    started gamemanager

---
+++ Assets/Manager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour
+{
+
+    int nbActiveRunners;
+    bool runnerLeftActive = true;
+    int MAX_NB_PLAYER = 4;
+    public GameObject joueur1,
+    joueur2,
+    joueur3,
+    joueur4;
+
+    void Start()
+    {
+        AssignLeader();
+    }
+
+    private void AssignLeader()
+    {
+        Random.Range(0, MAX_NB_PLAYER);
+    }
+
+    void Update()
+    {
+        if (nbActiveRunners == 0)
+        {
+            runnerLeftActive = false;
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        throw new System.NotImplementedException();
+    }
+}
